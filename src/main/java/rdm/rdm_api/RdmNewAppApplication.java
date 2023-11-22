00a299a2efb4bcbee565dc0cc6b5791deeba2717
AvinashGuru <31862054+AvinashGuru@users.noreply.github.com>
2023-11-22 11:12:56 +0530

    Add files via upload

---
+++ src/main/java/rdm/rdm_api/RdmNewAppApplication.java
@@ -0,0 +1,13 @@
+package rdm.rdm_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdmNewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdmNewAppApplication.class, args);
+	}
+
+}
